a9b5ad172757448b2e712721488fac4f07a0cb2a
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-14 15:04:30 +0300

    Update insert_into_h_currencies.sql

--- src/sql/insert_into_h_currencies.sql
+++ src/sql/insert_into_h_currencies.sql
@@ -1,1 +1,9 @@
-
+INSERT INTO STV230530__DWH.h_currencies(hk_currency_id, currency_code,date_update,load_dt,load_src)
+select
+       hash(currency_code) as  hk_currency_id,
+       currency_code,
+       date_update,
+       now() as load_dt,
+       's3' as load_src
+       from STV230530__STAGING.currencies
+where hash(currency_code) not in (select hk_currency_id from STV230530__DWH.h_currencies);
