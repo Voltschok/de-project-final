235219fd79df8d2d777e2936e485e92797559421
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-14 13:58:28 +0300

    Update and rename stg_settings.sql to transactions_update.sql

--- src/stg_settings.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE  (
-  key varchar not null,
-  update_ts datetime
-);
---
+++ src/transactions_update.sql
@@ -0,0 +1,4 @@
+CREATE TABLE STV230530__STAGING.transactions_update  (
+  key varchar not null,
+  update_ts datetime
+);
