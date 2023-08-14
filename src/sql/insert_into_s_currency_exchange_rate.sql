fce5484a1a5159386a48000c11f436bea6f322f9
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-14 16:37:33 +0300

    Update insert_into_s_currency_exchange_rate.sql

--- src/sql/insert_into_s_currency_exchange_rate.sql
+++ src/sql/insert_into_s_currency_exchange_rate.sql
@@ -1,1 +1,7 @@
-
+INSERT INTO STV230530__DWH.s_currency_exchange_rate(hk_currency_id  ,  currency_with_div , load_dt,load_src)
+select hc.hk_currency_id ,
+c.currency_with_div ,
+now() as load_dt,
+'s3' as load_src
+from STV230530__DWH.h_currencies as hc
+left join STV230530__STAGING.currencies as c on hc.currency_code  = c.currency_code;
