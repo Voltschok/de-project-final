534d9f006d88f6c5b4aa81beb549291479de4933
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-14 14:43:40 +0300

    Update 1_data_import.py

--- src/dags/1_data_import.py
+++ src/dags/1_data_import.py
@@ -51,10 +51,10 @@ conn_info = {'host': vertica_host,
 key_id= 'YCAJEWXOyY8Bmyk2eJL-hlt2K' #config.get('S3', 'aws_access_key_id')
 secret_key='YCPs52ajb2jNXxOUsL4-pFDL1HnV2BCPd928_ZoA' #config.get('S3', 'aws_secret_access_key')
 
-def load_data_postgres():
+def load_transactions_data_postgres(table:str)->None:
     with vertica_python.connect(**conn) as connection:
         cur_vertica = connection.cursor()  
-        cur_vertica.execute("SELECT max(update_ts) FROM  STV230530__STAGING.transactions_update")
+        cur_vertica.execute(f"SELECT max(update_ts) FROM  STV230530__STAGING.{table}_update")
         last_loaded_dt= cur_vertica.fetchone()
         cur_vertica.close()
 
@@ -62,7 +62,7 @@ def load_data_postgres():
         cursor = connect_to_postgresql.cursor()
         cur_postrgres = conn.cursor()
         input = io.StringIO()
-        cur_postrgres.copy_expert(f'''COPY (SELECT * from table WHERE transaction_dt > {last_loaded_dt} ORDER BY transaction_dt) TO STDOUT;''', input)
+        cur_postrgres.copy_expert(f'''COPY (SELECT * from public.{table} WHERE {operation_ts} > {last_loaded_dt} ORDER BY {operation_ts}) TO STDOUT;''', input)
         cur_postrgres.close()
       
     with vertica_python.connect(**conn) as connection:
@@ -73,48 +73,48 @@ def load_data_postgres():
         cur_vertica.connection.commit()
         cur_vertica.close()
     
+ 
+# def load_data(conn, path:str ,  file:str):  
+#     df_csv = pd.read_csv( path )
+#     tuple_col=", ".join(list(df_csv.columns) )
+#     tuple_col_str= ('('+ str(tuple_col)+')')
+#     print(tuple_col_str)
+#     with vertica_python.connect(**conn) as connection:
+#         cur = connection.cursor()
+#         cur.execute(f"""delete from STV230530__STAGING.{ file }""")
+#         connection.commit()
+#         cur.execute(f"""COPY STV230530__STAGING.{ file }{tuple_col_str}
+#             FROM LOCAL '{ path }' DELIMITER ',' ENFORCELENGTH""" )
+#         connection.commit()
+#         cur.close()
+        
+# def get_s3_file_list():
+#         s3_file_list=[]
+#         session = boto3.session.Session()
+#         s3_client = session.client(service_name='s3',
+#         endpoint_url='https://storage.yandexcloud.net',
+#         aws_access_key_id=key_id,
+#         aws_secret_access_key=secret_key)
+#         objects = s3_client.list_objects(Bucket='final-project')
+#         for object in objects['Contents']:
+#             s3_file_list.append(object['Key'])
+        
+#         return s3_file_list
 
-def load_data(conn, path:str ,  file:str):  
-    df_csv = pd.read_csv( path )
-    tuple_col=", ".join(list(df_csv.columns) )
-    tuple_col_str= ('('+ str(tuple_col)+')')
-    print(tuple_col_str)
-    with vertica_python.connect(**conn) as connection:
-        cur = connection.cursor()
-        cur.execute(f"""delete from STV230530__STAGING.{ file }""")
-        connection.commit()
-        cur.execute(f"""COPY STV230530__STAGING.{ file }{tuple_col_str}
-            FROM LOCAL '{ path }' DELIMITER ',' ENFORCELENGTH""" )
-        connection.commit()
-        cur.close()
+# def fetch_s3_file(bucket: str, key: str, bucket_quantity: int):
+#     # сюда поместить код из скрипта для скачивания файла
+#     files = os.listdir('/data/')
+#     for i in range(1, bucket_quantity+1):	
+#         if key=='transactions_batch_':
+#             key_boto3=key+str(i)+'.csv'
+#         else:
+#             key_boto3=key+'.csv'
+#         print(key)
         
-def get_s3_file_list():
-        s3_file_list=[]
-        session = boto3.session.Session()
-        s3_client = session.client(service_name='s3',
-        endpoint_url='https://storage.yandexcloud.net',
-        aws_access_key_id=key_id,
-        aws_secret_access_key=secret_key)
-        objects = s3_client.list_objects(Bucket='final-project')
-        for object in objects['Contents']:
-            s3_file_list.append(object['Key'])
-        
-        return s3_file_list
-
-def fetch_s3_file(bucket: str, key: str, bucket_quantity: int):
-    # сюда поместить код из скрипта для скачивания файла
-    files = os.listdir('/data/')
-    for i in range(1, bucket_quantity+1):	
-        if key=='transactions_batch_':
-            key_boto3=key+str(i)+'.csv'
-        else:
-            key_boto3=key+'.csv'
-        print(key)
-        
-        if key_boto3 not in files:
-            s3_client.download_file(bucket,
-            key_boto3,
-            Filename=f'/data/{key_boto3}')
+#         if key_boto3 not in files:
+#             s3_client.download_file(bucket,
+#             key_boto3,
+#             Filename=f'/data/{key_boto3}')
 
  
 with DAG('final_project_staging', schedule_interval=None, start_date=pendulum.parse('2022-10-01')
@@ -122,14 +122,14 @@ with DAG('final_project_staging', schedule_interval=None, start_date=pendulum.pa
  
 
     task1 = PythonOperator(
-         task_id='fetch_currencies',
-         python_callable=fetch_s3_file,
-         op_kwargs={'bucket': 'final-project', 'key': 'currencies_history', 'bucket_quantity':1},
+         task_id='load_transactions',
+         python_callable=load_transactions_data_postgres,
+         op_kwargs={'operation_ts': 'transaction_dt'},
      )
     task2 = PythonOperator(
-        task_id='fetch_transactions',
-        python_callable=fetch_s3_file,
-        op_kwargs={'bucket': 'final-project', 'key': 'transactions_batch_', 'bucket_quantity':10}
+        task_id='load_currencies',
+        python_callable=load_currencies_data_postgres,
+         op_kwargs={'operation_ts': 'transaction_dt'}, 
     )
  
     task3=PythonOperator(
