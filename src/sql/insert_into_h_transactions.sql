34651281811b4f09f4283d1f9af865f3b39c3a1d
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-14 15:04:46 +0300

    Update insert_into_h_transactions.sql

--- src/sql/insert_into_h_transactions.sql
+++ src/sql/insert_into_h_transactions.sql
@@ -1,1 +1,9 @@
-
+INSERT INTO STV230530__DWH.h_transactions(hk_transaction_id, transaction_id,transaction_dt,load_dt,load_src)
+select
+       hash(transaction_id) as  hk_transaction_id,
+       operation_id as transaction_id,
+       transaction_dt,
+       now() as load_dt,
+       's3' as load_src
+       from STV230530__STAGING.transactions
+where hash(transaction_id) not in (select hk_transaction_id from STV230530__DWH.h_transactions);
