6f520a6bb131f4c39117f26d677b69c03e40a298
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-15 15:17:25 +0300

    Update insert_into_global_metrics.sql

--- src/sql/insert_into_global_metrics.sql
+++ src/sql/insert_into_global_metrics.sql
@@ -4,19 +4,8 @@ INSERT INTO STV230530__DWH.global_metrics (date_update,
                                           cnt_transactions, 
                                           avg_transactions_per_account, 
                                           cnt_accounts_make_transactions)
-SELECT 
-  now()::data as date_update, currency, 
-  hc.currency_code as currency_from,
-  sum() as amount_total,
-  count(distinct transactions),
-  avg() as avg_transactions_per_account,  
-  count() as cnt_accounts_make_transactions
-  
-FROM STV230530__DWH.h_transactions ht
-LEFT JOIN STV230530__DWH.l_transaction_currency ltc on ht.hk_transaction_id=ltc.hk_transaction_id
-LEFT JOIN STV230530__DWH.h_currencies hc on hc.hk_currency_id=ltc.hk_currency_id
 
-  
+WITH temp_table AS(  
 SELECT now()::data as date_update, 
   ht.transaction_id, 
   hc.currency_code as currency_from, 
@@ -27,21 +16,26 @@ LEFT JOIN STV230530__DWH.s_transactions_amount sta on sta.hk_transaction_id = ht
 LEFT JOIN STV230530__DWH.l_transaction_currency ltc on ht.hk_transaction_id=ltc.hk_transaction_id
 LEFT JOIN STV230530__DWH.h_currencies hc on hc.hk_currency_id=ltc.hk_currency_id
 GROUP BY  date_update, ht.transaction_id
+)
 
+   
 SELECT now()::data as date_update, 
   ht.transaction_id, 
   hc.currency_code as currency_from, 
   sum(sta.amount) as amount_total
-  
+FROM STV230530__DWH.s_transactions_amount st
 FROM STV230530__DWH.h_transactions ht
-GROUP BY  date_update, ht.transaction_id
+
 
 SELECT now()::data as date_update, 
-       currency_from,
-      sum() as amount_total,
-      count(distinct transactions),
-      count(distinct transactions)( as avg_transactions_per_account,  
-      count() as cnt_accounts_make_transactions
+      currency_from,
+      sum(amount) as amount_total,
+      count(distinct hk_transaction_id),
+      count(distinct transactions)/count(distinct hk_account_id) as avg_transactions_per_account,  
+      count(distinct hk_account_id) as cnt_accounts_make_transactions
+FROM temp_table
+GROUP BY date_update, currency_from
+
 
 -- date_update — дата расчёта,
 -- currency_from — код валюты транзакции;
