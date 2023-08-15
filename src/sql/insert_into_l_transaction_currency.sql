2a4172694fc3911c7627a40f23049d1b407573db
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-15 16:46:04 +0300

    Update insert_into_l_transaction_currency.sql

--- src/sql/insert_into_l_transaction_currency.sql
+++ src/sql/insert_into_l_transaction_currency.sql
@@ -12,6 +12,6 @@ hc.hk_currency_id,
 now() as load_dt,
 's3' as load_src
 from STV230530__STAGING.transactions  as t
-left join STV230530__DWH.h_currencies  as hc on t.currency_code = hc.currency_code
+left join STV230530__DWH.h_currencies  as hc on t.currency_code = hc.currency_code and t.transaction_dt=hc.date_update 
 left join STV230530__DWH.h_transactions as ht on t.operation_id = ht.transaction_id
 where hash(ht.hk_transaction_id, hc.hk_currency_id) not in (select hk_l_transaction_currency from STV230530__DWH.l_transaction_currency);
