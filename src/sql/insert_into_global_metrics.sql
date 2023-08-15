cc693b8952ad860470098b0e7bf602a233a7e80d
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-08-15 17:21:14 +0300

    Update insert_into_global_metrics.sql

--- src/sql/insert_into_global_metrics.sql
+++ src/sql/insert_into_global_metrics.sql
@@ -8,11 +8,13 @@ INSERT INTO STV230530__DWH.global_metrics (date_update,
 WITH temp_transaction AS(  
 SELECT  
   ht.hk_transaction_id, 
-  hc.currency_code as currency from, 
-  sta.amount,
-  sts.status,
+  hc.currency_code as currency_from,
   scer.currency_with_div, 
-  lta.hk_account_id 
+  CASE WHEN  hc.currency_code=420 THEN sta.amount
+  ELSE sta.amount*scer.currency_with_div  END AS amount 
+  sts.status,
+  lta.hk_account_id,
+  ha.account_number_from
   
 FROM STV230530__DWH.h_transactions ht
 LEFT JOIN STV230530__DWH.l_transaction_currency ltc on ht.hk_transaction_id=ltc.hk_transaction_id
@@ -20,29 +22,17 @@ LEFT JOIN STV230530__DWH.h_currencies hc on hc.hk_currency_id=ltc.hk_currency_id
 LEFT JOIN STV230530__DWH.s_currency_exchange_rate scer on scer.hk_currency_id = hc.hk_currency_id
 LEFT JOIN STV230530__DWH.s_transactions_amount sta on sta.hk_transaction_id = ht.hk_transaction_id
 LEFT JOIN STV230530__DWH.l_transaction_account lta on ht.hk_transaction_id=lta.hk_transaction_id
+LEFT JOIN STV230530__DWH.h_accounts  as ha on lta.hk_account_id = ha.hk_account_id
 LEFT JOIN STV230530__DWH.s_transactions_status sts on sts.hk_transaction_id = ht.hk_transaction_id
 LEFT JOIN STV230530__DWH.s_transactions_type stt on stt.hk_transaction_id = ht.hk_transaction_id
   
 WHERE ht.transaction_td::DATE = '{{ ds }}'-1
 AND sts.status = 'done'
 AND stt."type" != 'authorisation'  
-),
-  
-temp_currency AS(
-  SELECT  
-  ht.transaction_id, 
-  hc.currency_code as currency_from, 
-  sum(sta.amount) as amount_total
-FROM STV230530__DWH.h_currencies hc on hc.hk_currency_id=ltc.hk_currency_id
-WHERE hc.date_update = '{{ ds }}'
-  )
+AND ha.account_number_from>0
+) 
 
-  
-   
-
-
-
-SELECT '{{ ds }}', --now()::data as date_update, 
+  SELECT '{{ ds }}', --now()::data as date_update, 
       currency_from,
       sum(amount) as amount_total,
       count(distinct hk_transaction_id),
